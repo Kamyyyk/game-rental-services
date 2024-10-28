e21bfc140d72d7bc21f5d37932eebaf5f50d9dc8
Kamil Chmielewski <kamil.chmielewski@silksh.pl>
2024-10-28 17:21:28 +0100

    first commit

---
+++ src/main/java/org/example/gamerentalservices/GameRentalServicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamerentalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRentalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRentalServicesApplication.class, args);
+	}
+
+}
