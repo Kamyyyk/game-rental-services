02f3d8726c06a5162879b82fc7fcf48f40f40f50
SkowronskiMarcin <skowronskimh@gmail.com>
2024-12-02 18:43:09 +0100

    Marcin_02.12

---
+++ rentalservice/src/main/java/pl/aeh/microservices/rentalservice/app/RentalOrder.java
@@ -0,0 +1,28 @@
+package pl.aeh.microservices.rentalservice.app;
+
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class RentalOrder {
+
+    private UUID id;
+    private UUID game_id; // ID wypożyczanej gry
+    private Date endDate; // Data końca wypożyczenia
+
+    public RentalOrder() {}
+    // Konstruktor
+    public RentalOrder(UUID id, UUID game, Date endDate) {
+        this.id = id;
+        this.endDate = endDate;
+    }
+
+
+}
+
