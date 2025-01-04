cc1d178b3c1419bd8ffc99ebdeaca830ad710988
Kamil Chmielewski <kamil.chmielewski@silksh.pl>
2025-01-04 13:07:14 +0100

    fixed security config java

--- apigateway/src/main/java/pl/aeh/microservices/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/pl/aeh/microservices/apigateway/config/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, Converter<Jwt, Mono<AbstractAuthenticationToken>> authenticationConverter) throws Exception {
         http.authorizeExchange(exchange -> exchange
-                    .pathMatchers("/actuator/").permitAll()
-                    .pathMatchers("/inventory/").hasRole("USER")
+                        .pathMatchers("/**").hasRole("USER")
+                        .anyExchange().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(authenticationConverter)));
 
         http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
